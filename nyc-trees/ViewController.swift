a944c0b542d7f3654a7d9cf295a9d361a6346538
suri <swigder@gmail.com>
2019-03-24 11:52:07 -0400

    Initial Commit

---
+++ nyc-trees/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nyc-trees
+//
+//  Created by Suri Wigder on 3/24/19.
+//  Copyright © 2019 Suri Wigder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
